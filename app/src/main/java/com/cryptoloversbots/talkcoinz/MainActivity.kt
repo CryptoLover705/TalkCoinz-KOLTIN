82664389f8c8d0ec0208f23222b02d5480ac9f96
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2020-03-02 20:42:02 +0000

    Installing Firebase Auth and Register User + Rounded Corners UI

--- app/src/main/java/com/cryptoloversbots/talkcoinz/MainActivity.kt
+++ app/src/main/java/com/cryptoloversbots/talkcoinz/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,13 +16,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         register_button_register.setOnClickListener {
-            val email = email_edittext_register.text.toString()
-            val password = password_edittext_register.text.toString()
-
-            Log.d("MainActivity", "Email is: " + email)
-            Log.d("MainActivity", "Password: $password")
-
-            // Firebase Authentication to create a user with email and password
+            performRegister()
         }
 
         already_have_account_text_view.setOnClickListener {
@@ -32,4 +28,31 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun performRegister() {
+        val email = email_edittext_register.text.toString()
+        val password = password_edittext_register.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter text in email/pw", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Log.d("MainActivity", "Email is: " + email)
+        Log.d("MainActivity", "Password: $password")
+
+        // Firebase Authentication to create a user with email and password
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+                // else if successful
+                Log.d("Main", "Successfully created user with uid: ${it.result!!.user!!.uid}")
+            }
+            .addOnFailureListener{
+                Log.d("Main", "Failed to create user: ${it.message}")
+                Toast.makeText(this, "Failed to create user: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
 }
