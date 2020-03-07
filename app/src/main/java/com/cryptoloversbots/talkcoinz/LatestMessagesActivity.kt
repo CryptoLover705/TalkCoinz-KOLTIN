f45ccc149c85a0548ffd31895554263d5dd4bc9b
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2020-03-07 14:30:12 +0000

    fixed a typo
    LatestMessageActivity ==>  LatestMessagsActivity
    
    activity_latest_message ==> activity_latest_messages

--- app/src/main/java/com/cryptoloversbots/talkcoinz/LatestMessagesActivity.kt
+++ app/src/main/java/com/cryptoloversbots/talkcoinz/LatestMessagesActivity.kt
@@ -3,10 +3,10 @@ package com.cryptoloversbots.talkcoinz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LatestMessageActivity : AppCompatActivity() {
+class LatestMessagesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_latest_message)
+        setContentView(R.layout.activity_latest_messages)
     }
 }
